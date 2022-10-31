1c87fade3f7acecedc8697586ad4671690c54a8a
Marco Crupi <marcocrupi@hotmail.it>
2022-10-31 17:58:39 +0100

    add exercise02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const h1 = <h1>Hello, World!</h1>;
+class App extends React.Component {
+
+  MyFunc(name) {
+    return <h1>Hello, {name}</h1>;
+  }
+
+  render() {
+    return <div>{this.MyFunc("Marco")}</div>;
+  }
 }
 
 export default App;
