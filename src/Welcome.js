58aa51eaeada6d529c6ddfc9786ba50bba06d0b4
Marco Crupi <marcocrupi@hotmail.it>
2022-11-02 07:25:42 +0100

    add exercise-props-05

---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Age from "./Age";
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}!</p>
+        <Age age={ this.props.age } />
+      </div>
+    );
+  }
+}
+
+export default Welcome;
