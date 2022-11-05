f2abbce6ef29c86a1d3e89b9c570a585df96cb89
Marco Crupi <marcocrupi@hotmail.it>
2022-11-05 08:04:29 +0100

    add exercise-state-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.changeCount = this.changeCount.bind(this);
+  }
+
+  // AUTOSTART SOLUTION
+  // componentDidMount() {
+  //   this.changeCount();
+  // }
+
+  changeCount() {
+    let counter = 0;
+    setInterval(() => {
+      this.setState({ count: counter++ });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.changeCount}>Start Counter</button>
+      </div>
+    );
+  }
 }
-
 export default App;
