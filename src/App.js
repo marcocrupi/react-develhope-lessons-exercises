3fca6e70b9ace67ae35fa7267e88f9f5d01d6230
marcocrupi <marcocrupi@hotmail.it>
2022-11-19 09:13:30 +0100

    add exercise-list-03

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import TodoList from "./TodoList";
 
-const items = [
-  { id: 1, title: "Buy Milk", completed: false },
-  { id: 2, title: "Go to the movies", completed: false },
-  { id: 3, title: "Water the plants", completed: true },
-  { id: 4, title: "Sleep", completed: true },
-];
-
 class App extends React.Component {
   render() {
     return (
       <div>
-        <TodoList items={items} />
+        <TodoList />
       </div>
     );
   }
 }
 
 export default App;
+
+
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,43 @@
 import React from "react";
 
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      id: 0,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: this.state.id,
+      label: e.target[0].value,
+    };
+
+    if (!newTodo.label) return;
+
+    this.setState((state) => ({
+      todos: [...state.todos, newTodo],
+      id: state.id + 1,
+    }));
+
+    e.target[0].value = "";
+  }
+
   render() {
     return (
       <div>
-        {/* Invece di ul e li posso anche renderizzare altri elementi html */}
+        <h1>Todo List</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="todo" />
+          <input type="submit" value="Add Todo" />
+        </form>
         <ul>
-          {this.props.todos.map((todo) => (
-            <li key={todo.id}>
-              {todo.title}
-              {todo.completed && <span>(COMPLETED)</span>}
-            </li>
+          {this.state.todos.map((todo) => (
+            <li key={todo.id}>{todo.label}</li>
           ))}
         </ul>
       </div>
