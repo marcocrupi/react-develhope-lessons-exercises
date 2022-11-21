b383a8b2619e0569399ad8002719980308ba0473
marcocrupi <marcocrupi@hotmail.it>
2022-11-21 07:10:49 +0100

    add exercise component composition 01

---
+++ src/Container.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./container.css"
+
+class Container extends React.Component {
+  render() {
+    return <div class="container"></div>;
+  }
+}
+
+export default Container;
