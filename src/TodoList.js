92f9fa28f43ab45bf7293d00734c5dcb5213fabb
marcocrupi <marcocrupi@hotmail.it>
2022-11-26 09:56:04 +0100

    add exercise render props 01

---
+++ src/TodoList.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      id: 0,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetTodos = this.resetTodos.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: this.state.id,
+      label: e.target[0].value,
+    };
+
+    if (!newTodo.label) return;
+
+    this.setState((state) => ({
+      todos: [...state.todos, newTodo],
+      id: state.id + 1,
+    }));
+
+    e.target[0].value = "";
+  }
+
+  resetTodos() {
+    this.setState(() => ({
+      todos: [],
+      id: 0,
+    }));
+  }
+
+  onDelete(id) {
+    const list = [...this.state.todos];
+    const updatedList = list.filter((todos) => todos.id !== id);
+    this.setState({
+      todos: updatedList,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="todo" />
+          <input type="submit" value="Add Todo" />
+        </form>
+        <button onClick={this.resetTodos}>Reset</button>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.label}
+              <button onClick={() => this.onDelete(todo.id)}>remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
