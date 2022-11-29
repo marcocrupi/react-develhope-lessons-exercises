36e504030ddec82be3d831968f68c45a2c968481
marcocrupi <marcocrupi@hotmail.it>
2022-11-29 08:01:39 +0100

    add exercise useeffect 04

---
+++ src/GitHubUser.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function GitHubUser({ username }) {
+  const [user, setUser] = useState(null);
+  const string = "In attesa del server...";
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => response.json())
+      .then((fetcheduser) => setUser(fetcheduser));
+  }, [username]);
+
+  return (
+    <div>
+      <h1>{user ? user.login : string}</h1>
+    </div>
+  );
+}
+
+export default GitHubUser;
