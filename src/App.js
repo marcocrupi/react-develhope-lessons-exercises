9f7bc69a9f8293bbe5f62af910b159e0677ebedf
marcocrupi <marcocrupi@hotmail.it>
2022-11-30 08:14:02 +0100

    add exercise 06

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { GitHubUserList } from "./GitHubUserList";
+import ShowGitHubUser from "./ShowGitHubUser";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <>
+                <h1>Not Found</h1>
+                <br />
+                <Link to="/">Return to Home</Link>
+              </>
+            }
+          />
+
+          <Route path="users/" element={<GitHubUserList />}>
+            <Route path=":username" element={<ShowGitHubUser />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
