4ca327555d8d035b1551eb1f40f03ee2f48a19ff
marcocrupi <marcocrupi@hotmail.it>
2022-12-01 09:46:58 +0100

    add exercise hooks 03-04

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import GitHubUser from "./GitHubUser";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      <GitHubUser username={username}></GitHubUser>
     </div>
   );
 }
